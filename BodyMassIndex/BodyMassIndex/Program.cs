532f64136d725048d4f116a8d268371f365ec4d4
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-02 15:37:43 +0300

    calculating BMI
    internship

---
+++ BodyMassIndex/BodyMassIndex/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            double index;
+
+            Console.WriteLine("Enter your weight: ");
+            double kilo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your height: ");
+            double boy = Convert.ToDouble(Console.ReadLine());
+
+            index = kilo / (boy * boy);
+
+            if (index <= 18)
+            {
+                Console.WriteLine("WEAK: You are below the ideal weight...");
+            }
+            else if (index > 18 && index <= 25)
+            {
+                Console.WriteLine("NORMAL: You are in the ideal weight range...");
+            }
+            else
+            {
+                Console.WriteLine("OBEZ: You are above your ideal weight...");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
